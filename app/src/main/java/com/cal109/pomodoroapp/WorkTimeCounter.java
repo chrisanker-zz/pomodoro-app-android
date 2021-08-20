7394d24fa0f34bc89b4cff2ff656bcb2e1c2f3cd
christian-anker-larsen <christian.anker.larsen@maersk.com>
2021-08-20 16:06:29 +0200

    Added new class with test

---
+++ app/src/main/java/com/cal109/pomodoroapp/WorkTimeCounter.java
@@ -0,0 +1,10 @@
+package com.cal109.pomodoroapp;
+
+public class WorkTimeCounter {
+    String backgroundColor;
+
+    public String setBackgroundColor(boolean b) {
+        backgroundColor = "Red";
+        return backgroundColor;
+    }
+}
---
+++ app/src/test/java/com/cal109/pomodoroapp/WorkTimeCounterTest.java
@@ -0,0 +1,16 @@
+package com.cal109.pomodoroapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WorkTimeCounterTest {
+    @Test
+    public void WorkTimeCountDown_Started_SetBackgroundRed(){
+        WorkTimeCounter workTimeCounter = new WorkTimeCounter();
+        boolean hasStarted = false;
+        assertEquals("Red", workTimeCounter.setBackgroundColor(hasStarted));
+    }
+
+}
